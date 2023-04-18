816b15c0a140ff6236d6658884bc2f37dc2d1b1d
leego0221 <leego0221@naver.com>
2023-04-18 19:05:51 +0900

    배경 수정

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
--- js/weather.js
+++ js/weather.js
@@ -10,8 +10,8 @@ function onGeoOk(position) {
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp} ℃`;
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 function onGeoError() {
